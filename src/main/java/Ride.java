f9fa04a1e6177c9e82b3b0c5740d0f5137a647eb
Fraser McKay <frasermckay97@gmail.com>
2018-05-07 16:35:50 +0100

    initial commit

---
+++ src/main/java/Ride.java
@@ -0,0 +1,26 @@
+public abstract class Ride {
+
+    private double price;
+    private int minAge;
+    private int height;
+
+    public Ride(double price, int minAge, int height){
+        this.price = price;
+        this.minAge = minAge;
+        this.height = height;
+
+    }
+
+
+    public double getPrice(double price) {
+        return price;
+    }
+
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
